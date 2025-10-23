605feb0f26d59fb88422ade73c453a4354c9d3a7
Carolina Rojas Rincon <crojas@STAFF.LAN>
2025-10-23 16:52:15 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        public string Name = "Mateo";
+
+        static void Main()
+        {
+            Program person = new Program();
+            Console.WriteLine($"Hello World {person.Name}");
+        }
+    }
+}
+
+
