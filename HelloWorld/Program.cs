cbfb1a1be52f6d0781a82203aa752a0b69c7060a
Carolina Rojas Rincon <crojas@STAFF.LAN>
2025-10-25 15:40:12 -0500

    Add new code for age calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,14 +2,30 @@
 {
     class Program
     {
-        public string Name = "Mateo";
-
         static void Main()
         {
-            Program person = new Program();
-            Console.WriteLine($"Hello World {person.Name}");
+            DateOnly dateConverted = new DateOnly();
+            string nameInput;
+            string birthdayInput;
+            Console.WriteLine("¡Hola bienvenido a el calculador de años!");
+            Console.WriteLine("Escribe tu nombre: ");
+            nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto conocerte {nameInput}");
+            Console.WriteLine("Escribe tu fecha de nacimiento en formato dd/mm/yy: ");
+            birthdayInput = Console.ReadLine();
+            bool isDateValid = DateOnly.TryParse(birthdayInput, out dateConverted);
+            if(isDateValid==false)  Console.WriteLine($"La fecha de nacimiento es invalida, usted nos envío este dato erroneo {birthdayInput}");
+            Console.WriteLine();
         }
     }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+    }
 }
+
 
 
